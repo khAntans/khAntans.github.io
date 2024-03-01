16241f5d82a162c59d96027b4b29432c69179f4d
Kristofers Harijs Antāns <kristofersantans@gmail.com>
2024-03-01 21:35:29 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,30 @@
+let activeObj = [];
+AFRAME.registerComponent('collider-check', {
+    dependencies: ['raycaster'],
+
+  
+    init: function () {
+      this.el.addEventListener('raycaster-intersection', function (event) {
+        console.log('Player hit something!');
+        activeObj.push(event.detail.els[0]);
+            event.detail.els[0].setAttribute('scale',{x:2,y: 2, z:2})
+
+
+      });
+      this.el.addEventListener('raycaster-intersection-cleared', function (event) {
+        console.log("exit");
+        
+      let last = activeObj[0];
+      last.setAttribute('scale',{x:1,y: 1, z:1});
+            activeObj.shift();
+        // }
+        //  event.detail.els[0].setAttribute('scale',{x:1,y: 1, z:1})
+        // console.log(event);
+       
+
+      });
+
+    }
+  });
+
+  
